32c79f52885fa43b25ccba4ed8b16a87c9197dbe
soitrang92 <soitrang_92@yahoo.com>
2012-10-21 12:30:05 +0700

    Update MyProject/src/demo/MainApp.java

--- MyProject/src/demo/MainApp.java
+++ MyProject/src/demo/MainApp.java
@@ -5,6 +5,6 @@ public class MainApp
 	public static void  main(String[] args)
 	{
 		System.out.println("Tran Dang Khoa");
-		System.out.println("may la thang ngu");
+		System.out.println("cai nay sai roi, phai sua lai");
 	}
 }
