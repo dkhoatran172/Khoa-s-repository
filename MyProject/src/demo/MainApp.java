82d69ea290b385d7f93f3a3e7b5c63fccfac7583
Hung <Hung@Hung-PC>
2012-10-21 12:24:38 +0700

    12

--- MyProject/src/demo/MainApp.java
+++ MyProject/src/demo/MainApp.java
@@ -5,6 +5,6 @@ public class MainApp
 	public static void  main(String[] args)
 	{
 		System.out.println("Tran Dang Khoa");
-		System.out.println("ngu nhu cho");
+		System.out.println("may la thang ngu");
 	}
 }
